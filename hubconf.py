1519b387ae83ee9dba2911d38951cee6b3764cd2
Jun Rekimoto <rekimoto@acm.org>
2023-12-18 02:04:25 +0900

    .

--- hubconf.py
+++
@@ -1,8 +0,0 @@
-dependencies = ["torch"]
-
-URLS = {
-    "hubert": "https://github.com/rkmt/wesper-demo/releases/download/v0.1/model-layer12-450000.pt",
-    "fastspeech-googletts": "https://github.com/rkmt/wesper-demo/releases/download/v0.1/googletts_neutral_best.tar",
-    "hifigan": "https://github.com/rkmt/wesper-demo/releases/download/v0.1/g_00205000",
-}
-
